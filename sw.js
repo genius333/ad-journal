fbd605eb89d4bf8d3bd2e77c7848a957b07cf3b1
genius333 <36622804+genius333@users.noreply.github.com>
2026-05-11 12:30:34 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'ad-journal-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest-journal.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => 
+      Promise.all(cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
